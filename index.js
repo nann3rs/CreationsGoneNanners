7709b1879dcb027257ba670390f81cd865b70d2b
Shannon Toft <srabena@gmail.com>
2019-10-17 13:55:49 -0700

    update About, Contact, and create Product page

--- index.js
+++ index.js
@@ -19,6 +19,48 @@ $(document).ready(function(){
 });
 
 
-$('.special.cards .image').dimmer({
-  on: 'hover'
-});
+let contents = `
+  
+    <div class="blurring dimmable image">
+      <div class="ui inverted dimmer">
+        <div class="content">
+          <div class="center">
+            <div class="ui primary button">Add Friend</div>
+          </div>
+        </div>
+      </div>
+      <img src="./images/alice.jpg">
+    </div>
+    <div class="content">
+      <a class="header">Alice in Wonderland Collection</a>
+      <div class="meta">
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="left floated">
+        $29.99
+        </span>
+        <span class="right floated star">
+        <i class="star icon"></i>
+          Favorite
+      </span>
+    </div>
+  
+`
+
+window.onload = function(){
+  let cardsDiv = document.querySelector('#cards')
+  let card = document.createElement('div')
+  card.classList.add("ui", "card")
+  card.innerHTML = contents
+  cardsDiv.appendChild(card) 
+}
+/*
+let data = {
+  title: Alice in Wonderland
+  header: 
+  price: $24.99
+  img: ./images/alice.jpg
+}
+
+*/
